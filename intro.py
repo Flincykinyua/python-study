fe8d7d8d2c442e3e437e29f23bec7558f769ccf8
FlincyKinyua <flincykinyua@gmail.com>
2024-10-14 17:03:09 +0300

    first commit

---
+++ intro.py
@@ -0,0 +1,25 @@
+#print("Flincy Murugi")
+# print  used to display the python output on the vs terminal
+#we use harsh to comment.
+fname="Flincy"
+fname="techcamp"
+print("techcamp")
+#wtite a program that adds 2 variables num1, num 2 and stores them in a variable called sum.
+num1=30
+num2=50
+sum=num1+num2
+print(sum)
+
+#declare 2 variable fname my name, lname add them up to get fullname
+fname="flincy"
+lname="murugi"
+fullname=fname+" "+lname
+print(fullname)
+
+#getting an input
+marks=input("enter your marks")
+print(marks)
+
+#input of phone number on the terminal
+phonenumber=input("enter your phone number")
+print(phonenumber)
